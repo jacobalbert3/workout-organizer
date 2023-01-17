fdd6b6619ffa133c8b6ed26ed399d65430045453
jacobalbert3 <122248719+jacobalbert3@users.noreply.github.com>
2023-01-16 20:17:55 -0500

    update

--- finding_workout.py
+++ finding_workout.py
@@ -14,26 +14,26 @@ def create_workout(muscle):
         reader = csv.reader(f)
         next(reader) # Skip the header row
         for row in reader:
-            muscle_group, workout, reps, link = row
+            muscle_group, workout, reps = row
             if muscle in muscle_group:
-                strength_exercises.append((workout, reps, link))
-            elif "Core" in muscle_group:
-                core_exercises.append((workout, reps, link))
+                strength_exercises.append((workout, reps))
+            elif "Abs" in muscle_group:
+                core_exercises.append((workout, reps))
             elif "Cardio" in muscle_group:
-                cardio_exercises.append((workout, reps, link))
+                cardio_exercises.append((workout, reps))
     
     # Choose 3 random strength exercises
-    strength_workout = random.sample(strength_exercises, 1)
+    strength_workout = random.sample(strength_exercises, 6)
+
     # Choose 2 random core exercises
-    core_workout = random.sample(core_exercises, 1)
+    core_workout = random.sample(core_exercises, 2)
     # Choose 2 random cardio
-    cardio_workout = random.sample(cardio_exercises, 1)
+    cardio_workout = random.sample(cardio_exercises, 2)
 
     workout = strength_workout + core_workout + cardio_workout
 
     list_of_exercizes =[]
-    #Print the workout
     for exercise in workout:
         list_of_exercizes.append(exercise[0] + " - " + exercise[1])
     return str(list_of_exercizes)
-    #return workout
+    #return string of workouts: the only way to input as a description for the GCAL
